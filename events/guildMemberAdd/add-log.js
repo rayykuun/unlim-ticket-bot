f653d2d89ecfd5c1cccf406eba2d9a5e0bf210f8
rayykuun <marvinpeschel13@web.de>
2024-07-10 18:47:46 +0200

    add server message if user join

--- events/guildMemberAdd/add-log.js
+++ events/guildMemberAdd/add-log.js
@@ -52,8 +52,39 @@ module.exports = async (member, client, handler) => {
 
     await memberLog.save();
 
-    console.log(`Mitglied ${member.user.tag} wurde erfolgreich geloggt.`);
+    const welcomeChannelId = "1260259517753458718"; // Ersetzen Sie dies durch die tatsächliche Channel-ID
+    const welcomeChannel = member.guild.channels.cache.get(welcomeChannelId);
+
+    if (welcomeChannel) {
+      const welcomeEmbed = new EmbedBuilder()
+        .setColor("#00FF00")
+        .setTitle("Willkommen auf unserem Server!")
+        .setImage(
+          "https://cdn.discordapp.com/attachments/1200530123007479949/1260637990196088892/image.png?ex=66900c1e&is=668eba9e&hm=bdde86066f83bc72e4949cc66e59ae360592418e4697489588bfe31cbf06971d&"
+        )
+        .setDescription(
+          `Hallo ${member},\n` +
+            "Bitte lies die Regeln und bestätige diese <#990767701402189824>.\n" +
+            "Hast du das erledigt wähle hier <#1180208160376631337> deine Rolle aus was du Spielen möchtest\n" +
+            "Damit wir dich auch richtig zu ordnen können mache bitte hier <#1200768896857280622> eine Anmeldung.\n\n" +
+            ".Wenn du beim starten im Spiel Hilfe brauchst kannst du dir gerne hier <#1003363731742412821> ein Starter beantragen!" +
+            "\n\n\nWir wünschen dir ganz viel Spaß auf unserem Server.🥳"
+        )
+        .setFooter({
+          text: `Mitglied Nr. ${member.guild.memberCount}`,
+        })
+        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }));
+
+      await welcomeChannel.send({ embeds: [welcomeEmbed] });
+    }
+
+    console.log(
+      `Mitglied ${member.user.tag} wurde erfolgreich geloggt und begrüßt.`
+    );
   } catch (error) {
-    console.error("Fehler beim Protokollieren des Mitglied-Beitritts:", error);
+    console.error(
+      "Fehler beim Protokollieren des Mitglied-Beitritts oder Senden der Begrüßung:",
+      error
+    );
   }
 };
